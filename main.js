0c7870dc54db5673a3c14bcbe64da4cbcb52a254
leo_ground <piscesljy@gmail.com>
2022-02-12 21:54:49 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -22,9 +23,19 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null) {
         return;
     }
+    navbarMenu.classList.remove('open');
     // console.log(event.target.dataset.link);
     scrollIntoViews(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    console.log('asdf')
+    navbarMenu.classList.toggle('open');
+});
+
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
