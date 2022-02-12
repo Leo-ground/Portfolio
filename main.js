edaf7b0caf4a0f1e60afd35bcca4427afbfbf20f
leo_ground <piscesljy@gmail.com>
2022-02-12 20:13:14 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -32,11 +32,6 @@ homeContactBtn.addEventListener('click',() => {
     scrollIntoViews('#contact');
 });
 
-function scrollIntoViews(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-}
-
 //Make home slowly fade to transparent as the window scrolls down 
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
@@ -44,4 +39,26 @@ document.addEventListener('scroll', () => {
     // console.log(1-window.scrollY / homeHeight);
     home.style.opacity = 1-window.scrollY / homeHeight;
 });
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector(".arrow-up")
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight/2 ) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+})
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoViews('#home');
+})
+
+function scrollIntoViews(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
+
 
